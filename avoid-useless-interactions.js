ac461996d945a1b155f2c700dcd905054de087c1
Christopher Anciaux <christopher.anciaux@gmail.com>
2020-12-02 18:13:54 +0100

    demonstration of appointment reservation in Doctolib

--- avoid-useless-interactions.js
+++ avoid-useless-interactions.js
@@ -11,10 +11,18 @@ async function findNantesDentistsDumbWay() {
     const page = await main.setupPage();
 
     await page.goto('https://doctolib.fr/');
+
+    // just for demonstration purposes
+    await page.waitForTimeout(2000);
+
     await page.click('#didomi-notice-agree-button');
     await page.fill('xpath=//input[@class="searchbar-input searchbar-query-input"]', 'Dentiste');
     await page.fill('xpath=//input[@class="searchbar-input searchbar-place-input"]', 'Nantes');
     await page.click('xpath=//div[@class="searchbar-place-submit"]//button[2]');
+
+    // just for demonstration purposes
+    await page.waitForTimeout(2000);
+
     await page.click('xpath=//div[@class="searchbar-place-submit"]//button[@type="submit"]');
 
     // Wait for navigation to page results
@@ -22,9 +30,21 @@ async function findNantesDentistsDumbWay() {
 
     // in results page
     await page.click('text=prochain rdv le');
+
+    // just for demonstration purposes
+    await page.waitForTimeout(2000);
+
     await page.click('.availabilities-slot');
+
+    // just for demonstration purposes
+    await page.waitForTimeout(3000);
+
     const option = await page.waitForSelector('#booking_motive > option:nth-child(2)', {state: 'attached'});
     await page.selectOption('#booking_motive', option);
+
+    // just for demonstration purposes
+    await page.waitForTimeout(2000);
+
     await page.click('.dl-step >> text=prendre rendez-vous');
 }
 
